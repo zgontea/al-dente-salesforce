9ffd989710b39280ebfe789dca092119ed6af4bc
Jan Gontarek <jgontarek@craftware.biz>
2023-03-11 16:23:13 +0100

    ALDENTE-001 (#1)
    * ALDENTE-001 create Patient object
    
    * ALDENTE-001 create Patient REST Controller
    
    * ALDENTE-001 add response wrappers class

---
+++ .sfdx/typings/lwc/sobjects/Patient__c.d.ts
@@ -0,0 +1,68 @@
+declare module "@salesforce/schema/Patient__c.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Patient__c.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Patient__c.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Patient__c.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/Patient__c.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Patient__c.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Patient__c.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Patient__c.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Patient__c.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Patient__c.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Patient__c.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Patient__c.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
+declare module "@salesforce/schema/Patient__c.First_Name__c" {
+  const First_Name__c:string;
+  export default First_Name__c;
+}
+declare module "@salesforce/schema/Patient__c.Last_Name__c" {
+  const Last_Name__c:string;
+  export default Last_Name__c;
+}
+declare module "@salesforce/schema/Patient__c.Birth_Date__c" {
+  const Birth_Date__c:any;
+  export default Birth_Date__c;
+}
+declare module "@salesforce/schema/Patient__c.Gender__c" {
+  const Gender__c:string;
+  export default Gender__c;
+}
+declare module "@salesforce/schema/Patient__c.PESEL__c" {
+  const PESEL__c:string;
+  export default PESEL__c;
+}
